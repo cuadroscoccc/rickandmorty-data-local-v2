297f86f895421dc3d6634c85ac5f24e80510cedd
cuadroscoccc <65979567+cuadroscoccc@users.noreply.github.com>
2020-10-02 15:53:51 -0500

    Delete index.js

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-
-  document.querySelector('.other-btnTwo').addEventListener('click', ()=>{
-  document.querySelector(".history").classList.toggle('active')
-  document.querySelector("footer").classList.toggle('active')
- 
-})
